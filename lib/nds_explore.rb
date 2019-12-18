76f1311b29929182ed2bb9a28390948479276214
Sergio Robles <sroblesc@gmail.com>
2019-12-18 17:37:17 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,16 +10,11 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
- puts directors_database[0][:movies][0][:title]
-  puts directors_database[0][:movies][1][:title]
-   puts directors_database[0][:movies][2][:title]
-    puts directors_database[0][:movies][3][:title]
-     puts directors_database[0][:movies][4][:title]
-  #row_index = 0
-  #src = []
-  #while row_index < directors_database[0][:movies].count do
-    #src << directors_database[0][:movies][row_index][:title]
-    #row_index += 1
-  #end
-  #return pp src
+  row_index = 0
+  src = []
+  while row_index < directors_database[0][:movies].count do
+    src << directors_database[0][:movies][row_index][:title]
+    row_index += 1
+  end
+  return puts src
 end
