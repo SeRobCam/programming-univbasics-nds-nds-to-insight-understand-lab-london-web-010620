51d3bb872c4cbd3cd2f14da171b74b8f364407e8
Sergio Robles <sroblesc@gmail.com>
2019-12-18 17:30:28 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,11 +10,5 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-  row_index = 0
-  src = []
-  while row_index < directors_database[0][:movies].count do
-    src << directors_database[0][:movies][row_index][:title]
-    row_index += 1
-  end
-  return pp src
+  pp
 end
